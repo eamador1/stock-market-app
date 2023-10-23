15b75b26ec83d2cb29b31fa01955d6ccfbd6e3a4
Erwin Amador <eamador0305@gmail.com>
2023-10-23 09:50:14 -0600

    v0.1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      God. Please Give me the intelligence, dilligenge and ability to finish this capstone on time!
+    </div>
+  );
+}
+
+export default App;

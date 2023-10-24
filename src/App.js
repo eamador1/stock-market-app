9a27483b2f5f88bf77b8712197736ff09c344a85
Erwin Amador <eamador0305@gmail.com>
2023-10-24 13:17:43 -0600

    makes first five routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import Euronext from './pages/Euronext';
+import LondonExchange from './pages/LondonExchange';
+import Nasdaq from './pages/Nasdaq';
+import Hke from './pages/Hke';
 
 function App() {
   return (
-    <div className="App">
-      God. Please Give me the intelligence, dilligenge and ability to finish this capstone on time!
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/euronext" element={<Euronext />} />
+        <Route path="/london-exchange" element={<LondonExchange />} />
+        <Route path="/nasdaq" element={<Nasdaq />} />
+        <Route path="/hke" element={<Hke />} />
+      </Routes>
+    </Router>
   );
 }
 

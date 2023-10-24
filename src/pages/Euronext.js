d7c4b5eed5c1eb3a91df79dd51d9318c81658a83
Erwin Amador <eamador0305@gmail.com>
2023-10-24 13:18:14 -0600

    give initial content to first four pages

---
+++ src/pages/Euronext.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Euronext = () => (
+  <div>
+    Euronext Page under Construction
+  </div>
+);
+
+export default Euronext;
---
+++ src/pages/Nasdaq.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nasdaq = () => (
+  <div>
+    Nasdaq Page under Construction
+  </div>
+);
+
+export default Nasdaq;

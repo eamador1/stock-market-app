c28fdb928b3f18fca6b8db40cf8d512b8968902c
Erwin Amador <eamador0305@gmail.com>
2023-10-27 10:30:12 -0600

    chage store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import companiesReducer from './companies/companiesSlice';
 
-const rootReducer = {
-  companies: companiesReducer,
-};
-
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    immutableCheck: false,
-    serializableCheck: false,
-  }),
+  reducer: {
+    home: companiesReducer,
+  },
 });
 
 export default store;

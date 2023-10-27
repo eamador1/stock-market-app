d9f5918679897c71d6bc3b0ffd5fb75a582d1fb3
Erwin Amador <eamador0305@gmail.com>
2023-10-27 10:25:07 -0600

    display full information available of selected company

---
+++ src/components/CompanyDetail.js
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SvgBack from '../imageComponent/back';
+import '../style/CompanyDetail.css';
+
+const Detail = () => {
+  const { data, id } = useSelector((state) => state.home);
+
+  return (
+    <div className="detailWrapper">
+      <div className="cross-container">
+        <Link to="/euronext" className="exit">
+          <SvgBack alt="exit" />
+        </Link>
+        <h3 className="infoHeader">COMPANYS INFO</h3>
+      </div>
+      <div className="adjuster">
+        <div className="content-container">
+          <h1>
+            {data[id].symbol}
+          </h1>
+          <h2>
+            {data[id].name}
+          </h2>
+          <h3>
+            Currency:
+            {' '}
+            {data[id].currency}
+          </h3>
+          <h3>
+            Stock Exchange:
+            { ' ' }
+            {data[id].stockExchange}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;

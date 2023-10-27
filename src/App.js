5d017449042f07454d9bcdecd790d4506e278c83
Erwin Amador <eamador0305@gmail.com>
2023-10-27 10:40:32 -0600

    import pages and create new routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Euronext from './pages/EuronextPage';
 import LondonExchange from './pages/LondonExchangePage';
 import Nasdaq from './pages/NasdaqPage';
 import Hke from './pages/HkePage';
+import Nse from './pages/NsePage';
+import Nyse from './pages/NysePage';
+import Detail from './components/CompanyDetail';
 
 function App() {
   return (
@@ -14,9 +17,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/euronext" element={<Euronext />} />
+        <Route path="/euronext/:content" element={<Detail />} />
         <Route path="/london-exchange" element={<LondonExchange />} />
         <Route path="/nasdaq" element={<Nasdaq />} />
         <Route path="/hke" element={<Hke />} />
+        <Route path="/nse" element={<Nse />} />
+        <Route path="/nyse" element={<Nyse />} />
       </Routes>
     </Router>
   );

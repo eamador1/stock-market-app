e832486db8af3ac321000d94d69b289595893af4
Erwin Amador <eamador0305@gmail.com>
2023-10-27 18:37:03 -0600

    delete EuronextPage.js

--- src/pages/EuronextPage.js
+++ src/pages/EuronextPage.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import Euronext from '../components/Euronext';
 
-function DisplayEuronext() {
-  return (
-    <>
-      <Euronext />
-    </>
-  );
-}
-export default DisplayEuronext;

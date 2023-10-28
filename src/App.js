180789e6d450239013ed0ee13feba179e61ffbd0
Erwin Amador <eamador0305@gmail.com>
2023-10-28 11:28:28 -0600

    delete imports and routes not needed any more

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
-import Euronext from './pages/EuronextPage';
-import LondonExchange from './pages/LondonExchangePage';
-import Nasdaq from './pages/NasdaqPage';
-import Hke from './pages/HkePage';
-import Nse from './pages/NsePage';
-import Nyse from './pages/NysePage';
 import Detail from './components/CompanyDetail';
 
 function App() {
@@ -16,13 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/euronext" element={<Euronext />} />
-        <Route path="/euronext/:content" element={<Detail />} />
-        <Route path="/london-exchange" element={<LondonExchange />} />
-        <Route path="/nasdaq" element={<Nasdaq />} />
-        <Route path="/hke" element={<Hke />} />
-        <Route path="/nse" element={<Nse />} />
-        <Route path="/nyse" element={<Nyse />} />
+        <Route path="/:content" element={<Detail />} />
       </Routes>
     </Router>
   );

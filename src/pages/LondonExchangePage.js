ee0cee7b6ada6b8ece9bd0758efe2ee73162c0e1
Erwin Amador <eamador0305@gmail.com>
2023-10-27 18:40:57 -0600

    delete LondonExchangePage.js

--- src/pages/LondonExchangePage.js
+++ src/pages/LondonExchangePage.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SvgBack from '../imageComponent/back';
-import '../App.css';
 
-const LondonExchange = () => (
-  <div>
-    <Link to="/" className="exit">
-      <SvgBack alt="exit" />
-    </Link>
-    <h2>
-      London Exchange Page under Construction
-    </h2>
-  </div>
-);
-
-export default LondonExchange;

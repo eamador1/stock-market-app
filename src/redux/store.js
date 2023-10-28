d7aaf7b4aa314b98976f8df8fb7e268fe6ede870
Erwin Amador <eamador0305@gmail.com>
2023-10-27 18:47:10 -0600

    delete store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,1 @@
-import { configureStore } from '@reduxjs/toolkit';
-import companiesReducer from './companies/companiesSlice';
 
-const store = configureStore({
-  reducer: {
-    home: companiesReducer,
-  },
-});
-
-export default store;

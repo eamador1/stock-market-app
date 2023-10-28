35b18777042b8855ec2b2a44e1be04b9b440654b
Erwin Amador <eamador0305@gmail.com>
2023-10-27 18:47:29 -0600

    add store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import companiesReducer from './companies/companiesSlice';
 
+const store = configureStore({
+  reducer: {
+    home: companiesReducer,
+  },
+});
+
+export default store;
